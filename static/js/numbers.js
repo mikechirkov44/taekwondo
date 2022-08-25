cf771e861accb0ebe857fadf83847faca1cb4105
Symegu <89914120+Symegu@users.noreply.github.com>
2022-08-25 18:12:45 +0300

    numbers

--- static/js/numbers.js
+++ static/js/numbers.js
@@ -1,31 +1,25 @@
-function random(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-  
-  function range(cur, next, count) {
-    // ищем интервал между промежуточными значениями, если интервал слишком маленький устанавливаем 1
-    var diff = Math.floor((next - cur) / count) || 1;
-  
-    // получаем промежуточные значения
-    var res = [cur];
-    for (var i = cur + diff; Math.abs(i - next) > Math.abs(diff); i += diff) {
-      res.push(i);
-    }
-    res.push(next);
-  
-    return res;
-  }
-  
-  var timer = null
-  
-  function generate(min, max) {
-    window.clearInterval(timer);
-    var current = +out.value;
-    var next = random(min, max);
-    var rng = range(current, next, 25);
-    timer = setInterval(() => {
-      // на каждой итерации берем следующее значение, пока они не закончатся.
-      out.value = rng.shift();
-      if (!rng.length) window.clearInterval(timer);
-    }, 10);
-  }
+const numbers = document.querySelectorAll("span.promo-list-item__red")
+
+function render() {
+  let interval = setInterval(() => {
+    numbers.forEach(element => {
+      let newnum = getRandomInt(0, +element.innerHTML+5);
+      element.classList.add("visible");
+      return element.innerHTML = newnum;
+    });
+  }, 40);
+  setTimeout(() => {
+    window.clearInterval(interval);
+    numbers.forEach(element => {
+        return element.innerHTML = element.dataset.value;
+    });
+  }, 800);
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+render(numbers)
