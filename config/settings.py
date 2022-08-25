a6159059f7690043682dc15f0c9841af2a3c50fa
mikechirkov44 <90888317+mikechirkov44@users.noreply.github.com>
2022-08-25 16:58:50 +0300

    Stage 2 (#5)
    * news model a added
    
    * news model added
    
    * admin cabinet settings
    
    * admin settings

--- config/settings.py
+++ config/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [f'{BASE_DIR}/templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [f'{BASE_DIR}/static/']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
